a1e355a846e58fe5c046a6b2d5ad576f0de55d26
pandabrand <pandabrand@gmail.com>
2018-03-29 17:06:09 -0500

    adding custom fields to plugin.

--- acantrell-custom-plugin.php
+++ acantrell-custom-plugin.php
@@ -121,7 +121,210 @@ function publication_post_type() {
 }
 add_action( 'init', 'publication_post_type', 0 );
 
-function acf_google_map_init() {
-	acf_update_setting('google_api_key', 'AIzaSyAy-5P5-TbICxrL2wqMuSWxM3IGhYP4Wjw');
-}
-add_action('acf/fields/google_map/api', 'acf_google_map_init');
+if( function_exists('acf_add_local_field_group') ):
+
+	acf_add_local_field_group(array(
+		'key' => 'group_5abd5c0b4e797',
+		'title' => 'Project Fields',
+		'fields' => array(
+			array(
+				'key' => 'field_5abd5c2bbd581',
+				'label' => 'Start Date',
+				'name' => 'start_date',
+				'type' => 'date_picker',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '50',
+					'class' => '',
+					'id' => '',
+				),
+				'display_format' => 'F j, Y',
+				'return_format' => 'F j, Y',
+				'first_day' => 0,
+			),
+			array(
+				'key' => 'field_5abd5c7dbd582',
+				'label' => 'Start Time',
+				'name' => 'start_time',
+				'type' => 'time_picker',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '50',
+					'class' => '',
+					'id' => '',
+				),
+				'display_format' => 'g:i a',
+				'return_format' => 'g:i a',
+			),
+			array(
+				'key' => 'field_5abd5c93bd583',
+				'label' => 'End Date',
+				'name' => 'end_date',
+				'type' => 'date_picker',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '50',
+					'class' => '',
+					'id' => '',
+				),
+				'display_format' => 'F j, Y',
+				'return_format' => 'F j, Y',
+				'first_day' => 0,
+			),
+			array(
+				'key' => 'field_5abd5ca3bd584',
+				'label' => 'End Time',
+				'name' => 'end_time',
+				'type' => 'time_picker',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '50',
+					'class' => '',
+					'id' => '',
+				),
+				'display_format' => 'g:i a',
+				'return_format' => 'g:i a',
+			),
+			array(
+				'key' => 'field_5abd5cb7bd585',
+				'label' => 'Place',
+				'name' => 'place',
+				'type' => 'text',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => 'Add a Place Name here...',
+				'prepend' => '',
+				'append' => '',
+				'maxlength' => '',
+			),
+			array(
+				'key' => 'field_5abd5cd7bd586',
+				'label' => 'Place Address',
+				'name' => 'place_address',
+				'type' => 'text',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'maxlength' => '',
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'project',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => 1,
+		'description' => '',
+	));
+	
+	acf_add_local_field_group(array(
+		'key' => 'group_5abd5d15c5e2e',
+		'title' => 'Publication Fields',
+		'fields' => array(
+			array(
+				'key' => 'field_5abd5d257cb2a',
+				'label' => 'Publication Link',
+				'name' => 'publication_link',
+				'type' => 'url',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+			),
+			array(
+				'key' => 'field_5abd62e702c3b',
+				'label' => 'Publication Link Name',
+				'name' => 'publication_link_name',
+				'type' => 'text',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'maxlength' => '',
+			),
+			array(
+				'key' => 'field_5abd5d3a7cb2b',
+				'label' => 'Publication Date',
+				'name' => 'publication_date',
+				'type' => 'date_picker',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'display_format' => 'm/d/Y',
+				'return_format' => 'm/d/Y',
+				'first_day' => 0,
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'publication',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => 1,
+		'description' => '',
+	));
+	
+	endif;
